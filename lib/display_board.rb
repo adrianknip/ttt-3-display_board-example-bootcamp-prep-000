5a2031fc43f339233340f45d08ce888c197ab368
nip.adrian@gmail.com Nip <nip.adrian@gmail.com>
2018-06-20 22:42:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  row1 = [" ","|"," ","|"," "];
-  gap = "-----------"
-  row2 = [" ","|"," ","|"," "];
-  row3 = [" ","|"," ","|"," "];
+  # row1 = [" ","|"," ","|"," "];
+  # gap = "-----------"
+  # row2 = [" ","|"," ","|"," "];
+  # row3 = [" ","|"," ","|"," "];
   puts row1
   puts gap
   puts row2
